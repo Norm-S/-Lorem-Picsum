0ca623f4ec55baf56947579a5d050294284caec2
shum_n <shumnorman@gmail.com>
2024-02-09 19:10:25 -0800

    Add selectRandomImage()

--- js/script.js
+++ js/script.js
@@ -3,10 +3,18 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-async function getImage() {
+const getImage = async function () {
   const res = await fetch("https://picsum.photos/v2/list?limit=100");
   const images = await res.json();
-  console.log(images);
-}
+  //   console.log(images);
+  selectRandomImage(images);
+};
+
+const selectRandomImage = function (images) {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  //   console.log(randomIndex);
+  const randomImage = images[randomIndex];
+  //   console.log(randomImage);
+};
 
 getImage();
